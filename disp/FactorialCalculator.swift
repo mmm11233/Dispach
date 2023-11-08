17639945954445bb824e3c532f25c424fc2da95d
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-11-09 00:51:16 +0400

    all  changes

--- disp/FactorialCalculator.swift
+++ disp/FactorialCalculator.swift
@@ -7,13 +7,35 @@
 
 import Foundation
 
-class FactorialCalculator {
-    static func factorial(of number: Int, completion: @escaping (Int) -> Void) {
-        var result = 1
-        for i in 1...number {
-            result *= i
+final class FactorialCalculator {
+    static func factorial(_ n: Int,  completion: @escaping (String) -> Void) {
+        var result = [1]
+        for i in 2...n {
+            result = result.map { $0 * i }
+            result = carryAll(result)
         }
-        completion(result)
+
+        completion(result.map(String.init).joined())
+    }
+    
+    static func carryAll(_ arr: [Int]) -> [Int] {
+        var result = [Int]()
+
+        var carry = 0
+        for val in arr.reversed() {
+            let total = val + carry
+            let digit = total % 10
+            carry = total / 10
+            result.append(digit)
+        }
+
+        while carry > 0 {
+            let digit = carry % 10
+            carry = carry / 10
+            result.append(digit)
+        }
+
+        return result.reversed()
     }
 }
 
--- disp/ViewController.swift
+++ disp/ViewController.swift
@@ -4,16 +4,53 @@
 //
 //  Created by Mariam Joglidze on 08.11.23.
 //
-
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calculateFactorials()
     }
-
-
+    
+   private func calculateFactorials() {
+        let firstNumber: Int = Int.random(in: 20...50)
+        let secondNumber: Int = Int.random(in: 20...50)
+        
+        var result1: String?
+        var result2: String?
+        
+        let group = DispatchGroup()
+        
+        group.enter()
+        DispatchQueue.global().async(group: group) {
+            FactorialCalculator.factorial(firstNumber) { result in
+                result1 = result
+                group.leave()
+            }
+        }
+        
+        group.enter()
+        DispatchQueue.global().async(group: group) {
+            FactorialCalculator.factorial(secondNumber) { result in
+                result2 = result
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: .main) {
+            if let result1 = result1, let result2 = result2 {
+                if result1 > result2 {
+                    print("Thread firts is the winner.")
+                } else if result2 > result1 {
+                    print("Thread second is the winner.")
+                } else {
+                    print("It's a tie!")
+                }
+            }
+        }
+    }
 }
 
